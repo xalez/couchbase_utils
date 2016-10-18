2c095772e4383b57d4cd8f38bfa7f494fd45cab7
Aliaksandr Zlobich <Aliaksandr_Zlobich@epam.com>
2016-10-18 10:24:41 +0300

    Allow to specify connection params and file

--- cb_import.py
+++ cb_import.py
@@ -1,10 +1,20 @@
+import argparse
 import json
 
 from couchbase.bucket import Bucket
 
-bucket = Bucket("couchbase://localhost/default")
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--server", default="localhost",
+                    help="specify server host")
+parser.add_argument("-b", "--bucket", default="default",
+                    help="bucket name")
+parser.add_argument("file_name", nargs='?', default="data.json",
+                    help="Name of file which contains data for import")
+args = parser.parse_args()
 
-with open('data.json') as data_file:
+bucket = Bucket("couchbase://" + args.server + "/" + args.bucket)
+
+with open(args.file_name) as data_file:
     data = json.load(data_file)
 
 for key, value in data.iteritems():
